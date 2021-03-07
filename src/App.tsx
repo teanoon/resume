c6d7335d4e25da0ac53e86adccdc847b0e106310
teanoon <anticipationvu@gmail.com>
2021-03-08 02:21:13 +0800

    print properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import './App.scss'
-
 import { Col, Row, Space, Tag, Typography } from 'antd'
-import React, { FC } from 'react'
+import React, { FC, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
-
+import { useReactToPrint } from 'react-to-print'
+import './App.scss'
 import Introduction from './components/Introduction/Introduction'
 import PersonalInformation from './components/PersonalInformation/PersonalInformation'
 import Statistics from './components/Statistics/Statistics'
 import Works from './components/Works/Works'
 
+
+
 const { Link, Paragraph } = Typography
 
 const App: FC = () => {
   const { t, i18n } = useTranslation()
+  const ref = useRef(null)
+  const handlePrint = useReactToPrint({
+    content: () => ref.current
+  })
   return (
-    <>
+    <div ref={ref}>
       <Space direction='vertical' size='large' style={{ padding: 16 }}>
         <Row gutter={[16, 16]}>
           <Col sm={24} md={24} xxl={{ span: 12, offset: 6 }} style={{ textAlign: 'right' }}>
@@ -45,12 +50,16 @@ const App: FC = () => {
         <Row gutter={[16, 16]}>
           <Col sm={24} md={24} xxl={{ span: 12, offset: 6 }}>
             <Paragraph style={{ textAlign: 'center' }}>
-              live @ <Link href='https://teanoon.github.io/resume' target='_blank'>https://teanoon.github.io/resume</Link>
+              <Space>
+                live @ <Link href='https://teanoon.github.io/resume' target='_blank'>https://teanoon.github.io/resume</Link>
+                or
+                <Link onClick={handlePrint}>print</Link>
+              </Space>
             </Paragraph>
           </Col>
         </Row>
       </Space>
-    </>
+    </div>
   )
 }
 
